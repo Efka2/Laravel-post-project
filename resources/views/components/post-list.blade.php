15c124aa75a4338d101d3a3f7c9c98986edb13df
Evald Šilobrit <Efka2@users.noreply.github.com>
2020-12-29 15:44:59 +0200

    awards

--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -1,36 +1,41 @@
-
-        
-
 <div class="m-3 p-4 bg-gray-200 h-1/2">
-        <div class="flex">
-            <div class="justify-center w-1/2 h-1/2">
+
+    <div class=" mr-2 text-xl mb-5">
+        <div class="inline ">
+            @if ($post->user->profile_picture)
+                <img class="mr-4 rounded-full inline-block " style="width: 10%" src=" {{ url( 'storage/profilePictures', [$post->user->id , $post->user->profile_picture])}} " />
+            @else
+                <img class="mr-4 rounded-full inline-block" style="width: 10%" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.watsonmartin.com%2Fwp-content%2Fuploads%2F2016%2F03%2Fdefault-profile-picture.jpg&f=1&nofb=1" />
+            @endif
+        </div>
+        <div class="inline-block">
+            <a style="color:blue" href=" {{route('user.profile', $post->user )}} ">{{$post->user->username}}</a>
+            {{$post->created_at -> diffForHumans() }}
+        </div>
+    </div>
+        <h1 class=" text-4xl font-semibold mb-2 ">{{$post->header}}</h1></br>
+        <div class="flex justify-center">
+            <div class=" w-1/2 h-1/2">
                 @if ($post->picture)
-                <img style="max-height: 50%" src=" {{ url( 'storage/avatars', [$post->user_id , $post->picture])}} " />
+                <img style="max-height: 50%;" src=" {{ url( 'storage/avatars', [$post->user_id , $post->picture])}} " />
                 @endif
             </div>
-            
-            <div class="justify-between mx-auto">
-                <h1 class=" text-4xl font-semibold mb-2 ">{{$post->header}}</h1></br>
-                
-                <div class=" text-xl">
-                    {{$post->body}}
-                </div>
-            </div>
-        
+
         </div>
 
         <div class="relative flex justify-start py-4 my-2 text-xl">
             <div class="mr-4">
-                <a href=" {{route('posts.view',$post)}} "><button 
+                <a href=" {{route('posts.view',$post)}} "><button
                  class="bg-gray-600 text-white font-bold py-2 px-4 hover:bg-gray-400 rounded">
-                    View 
+                    View
                 </button></a>
             </div>
+            <div class="inline-block">
             <!--LIKE-->
             @if( !$post->isLiked(auth()->user()) )
                 <form  action="{{route('posts.like',$post)}}" method="POST">
                     @csrf
-                    <button onclick="confirmation() type="submit" class="bg-gray-600 text-white font-bold py-2 px-4 hover:bg-gray-400 rounded">Like <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                    <button  type="submit" class="bg-gray-600 text-white font-bold py-2 px-4 hover:bg-gray-400 rounded">Like <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
                     </button>
                     <span> {{$post->likes->count()}} {{Str::plural('like',$post->likes->count())}} </span>
                 </form>
@@ -48,20 +53,31 @@
                     <form onclick="confirmation()" action="{{route('post.delete', $post)}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button onclick="confirmation()" type="submit" class="mx-4 bg-red-600 text-white font-bold py-2 px-4 hover:bg-red-400 rounded">Delete</button>
+                        <button  type="submit" class="mx-4 bg-red-600 text-white font-bold py-2 px-4 hover:bg-red-400 rounded">Delete</button>
                     </form>
                     <i class="fa fa-trash"></i>
                 @endcan
-                <div class="absolute bottom-0 right-0 mr-2 text-xl"> {{$post->created_at -> diffForHumans() }}
-                    by <a style="color:blue" href=" {{route('user.profile', $post->user )}} ">{{$post->user->username}}</a>
-                </div>   
+                </div>
+                <div class="ml-4">
+                    comments {{$post->comments->count()}}
+                </div>
+        </div>
+        <div class="">
+            @if ($post->awards->count())
+            Awards:
+                @foreach ($post->awards as $award)
+                    <img style="width:7%" src=" {{ url( 'storage/awards', [$award->name , $award->picture])}} " />
+                @endforeach
+
+            @endif
+
         </div>
 </div>
 
 <script>
     function confirmation(){
         if(confirm("Are you sure you want to delete this post?")){
-            
+
         }
         else event.preventDefault();
     }
